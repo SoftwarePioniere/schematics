01abe8d8c77a0fea8e8ef932c859a2933fcf5d50
Torsten Zwoch <tz@softwarepioniere.de>
2018-06-12 14:15:30 +0200

    1.0.17 Upgrade effect syntax from 4.x to current version

--- src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
+++ src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
@@ -5,10 +5,11 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/takeUntil';
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import {Effect, Actions, ofType} from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import {switchMap} from 'rxjs/operators';
 
 import * as ac from './actions.<%= actiontype %>.<%= dasherize(methodWithoutType) %>';
 import * as api from '<%= importpath %>clients/<%= clientname %>';
@@ -17,21 +18,21 @@ import { CommunicationService } from '<%= importpath %>sopi/providers/communicat
 
 @Injectable()
 export class <%= classify(clientname) %>Modul<%= classify(methodWithoutType) %><%= classify(methodWithoutType) %>Effects  {
-@Effect()
-    <%= classify(methodWithoutType) %>$: Observable<Action> = this.actions$.pipe(
-        ofType(ac.<%= underscore(classify(method)).toUpperCase() %>)
-        ,switchMap((x: ac.<%= classify(method) %>Action) => {
-            return this._<%= camelize(classify(service)) %>Service.<%= method %>(<%= requestparamsVariableNames %>)
-                .map((result:any) => {
-                    this._communicationService.requestSucceded(result,<% if (requestparamsVariableNamesSucceed !='') {%><%= requestparamsVariableNamesSucceed %><% }else{ %> null <% } %>);
-                    return new ac.<%= classify(method) %>ErfolgreichAction(<% if(responseparamsVariableNames!='') {%>result<% } %><% if (requestparamsVariableNames !='' && responseparamsVariableNames!='') {%>, <% } %><%= requestparamsVariableNames %>);
-                })
-                .catch((error:any) => {
-                    this._communicationService.requestError(error<% if (requestparamsVariableNamesSucceed !='') {%>, <% } %><%= requestparamsVariableNamesSucceed %>);
-                    return of(new ac.<%= classify(method) %>FehlerAction(error<% if (requestparamsVariableNames !='') {%>, <% } %><%= requestparamsVariableNames %>));
-                });
-    })
-);
+    @Effect()
+        <%= classify(methodWithoutType) %>$: Observable<Action> = this.actions$.pipe(
+            ofType(ac.<%= underscore(classify(method)).toUpperCase() %>),
+            switchMap((x: ac.<%= classify(method) %>Action) => {
+                return this._<%= camelize(classify(service)) %>Service.<%= method %>(<%= requestparamsVariableNames %>)
+                    .map((result: any) => {
+                        this._communicationService.requestSucceded(result,<% if (requestparamsVariableNamesSucceed !='') {%><%= requestparamsVariableNamesSucceed %><% }else{ %> null <% } %>);
+                        return new ac.<%= classify(method) %>ErfolgreichAction(<% if(responseparamsVariableNames!='') {%>result<% } %><% if (requestparamsVariableNames !='' && responseparamsVariableNames!='') {%>, <% } %><%= requestparamsVariableNames %>);
+                    })
+                    .catch((error: any) => {
+                        this._communicationService.requestError(error<% if (requestparamsVariableNamesSucceed !='') {%>, <% } %><%= requestparamsVariableNamesSucceed %>);
+                        return of(new ac.<%= classify(method) %>FehlerAction(error<% if (requestparamsVariableNames !='') {%>, <% } %><%= requestparamsVariableNames %>));
+                    });
+        })
+    );
 
     constructor(
         private actions$: Actions,
