56070e55d5bcf6ced995be6ed016e9130b8714c5
Torsten Zwoch <tz@softwarepioniere.de>
2019-02-07 00:36:41 +0100

    clean up

--- src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
+++ src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
@@ -9,26 +9,31 @@ import {Effect, Actions, ofType} from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import {switchMap} from 'rxjs/operators';
+import {filter, switchMap} from 'rxjs/operators';
 
 import * as ac from './actions.<%= actiontype %>.<%= dasherize(methodWithoutType) %>';
 import * as api from '<%= importpath %>clients/<%= clientname %>';
 import { CommunicationService } from '<%= importpath %>sopi/providers/communication.service';
-
+import {RequestMethod, RequestStackService, RequestType} from '<%= importpath %>sopi/providers/request-stack.service';
 
 @Injectable()
 export class <%= classify(clientname) %><%= classify(methodWithoutType) %>Effects  {
     @Effect()
         <%= classify(methodWithoutType) %>$: Observable<Action> = this.actions$.pipe(
             ofType(ac.<%= underscore(classify(method)).toUpperCase() %>),
+            filter((x: ac.GetTourFahrzeugNaechsteAction) => {
+                return this.requestStackService.callRequest(ac.<%= underscore(classify(method)).toUpperCase() %>, x, RequestMethod.<%= method.toUpperCase() %>, RequestType.Anfrage);
+            }),
             switchMap((x: ac.<%= classify(method) %>Action) => {
                 return this._<%= camelize(classify(service)) %>Service.<%= method %>(<%= requestparamsVariableNames %>)
                     .map((result: any) => {
                         this._communicationService.requestSucceded(result, <% if (requestparamsVariableNamesSucceed !='') {%><%= requestparamsVariableNamesSucceed %><% }else{ %> null <% } %>, x.optPayload);
+                        this.requestStackService.checkRequest(ac.<%= underscore(classify(method)).toUpperCase() %>, x, RequestMethod.<%= method.toUpperCase() %>, RequestType.Erfolgreich);
                         return new ac.<%= classify(method) %>ErfolgreichAction(<% if(responseparamsVariableNames!='') {%>result<% } %><% if (requestparamsVariableNames !='' && responseparamsVariableNames!='') {%>, <% } %><%= requestparamsVariableNames %>, x.optPayload);
                     })
                     .catch((error: any) => {
                         this._communicationService.requestError(error<% if (requestparamsVariableNamesSucceed !='') {%>, <% } %><%= requestparamsVariableNamesSucceed %>, x.optPayload);
+                        this.requestStackService.checkRequest(ac.<%= underscore(classify(method)).toUpperCase() %>, x, RequestMethod.<%= method.toUpperCase() %>, RequestType.Fehler, error);
                         return of(new ac.<%= classify(method) %>FehlerAction(error<% if (requestparamsVariableNames !='') {%>, <% } %><%= requestparamsVariableNames %>, x.optPayload));
                     });
         })
@@ -37,6 +42,7 @@ export class <%= classify(clientname) %><%= classify(methodWithoutType) %>Effect
     constructor(
         private actions$: Actions,
         private _<%= camelize(classify(service)) %>Service: api.<%= classify(service) %>Service,
-        private _communicationService: CommunicationService) {
+        private _communicationService: CommunicationService,
+        private requestStackService: RequestStackService) {
     }
 }
