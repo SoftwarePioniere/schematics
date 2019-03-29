f06995d00d87985df95b4886c4d1987c32e15987
Torsten Zwoch <tz@softwarepioniere.de>
2019-03-29 10:37:18 +0100

    clean up

--- src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/actions.__actiontype__.__methodWithoutType@dasherize__.ts
+++ src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/actions.__actiontype__.__methodWithoutType@dasherize__.ts
@@ -1,21 +1,21 @@
-import {Action} from '@ngrx/store';
 <% if (apiIsUsed !='') {%> import * as api from '<%= importpath %>clients/<%= clientname %>'; <% } %>
+import {NgrxManagerAction, NgrxManagerConfig} from '@softwarepioniere/ngrx-manager';
 
 export const <%= underscore(classify(method)).toUpperCase() %> = '[<%= classify(clientname) %>Modul] <%= classify(method) %> laden';
 export const <%= underscore(classify(method)).toUpperCase() %>_ERFOLGREICH = '[<%= classify(clientname) %>Modul] <%= classify(method) %> laden erfolgreich';
 export const <%= underscore(classify(method)).toUpperCase() %>_FEHLER = '[<%= classify(clientname) %>Modul] <%= classify(method) %> Ladefehler';
 
-export class <%= classify(method) %>Action implements Action {
+export class <%= classify(method) %>Action implements NgrxManagerAction {
     readonly type = <%= underscore(classify(method)).toUpperCase() %>;
-    constructor(<%= requestparams %><% if (requestparams !='') {%>, <% } %><%= optPayload %>) {}
+    constructor(<%= requestparams %><% if (requestparams !='') {%>, <% } %><%= optPayload %>, public ngrxManager: NgrxManagerConfig = null) {}
     }
-export class <%= classify(method) %>ErfolgreichAction implements Action {
+export class <%= classify(method) %>ErfolgreichAction implements NgrxManagerAction {
     readonly type = <%= underscore(classify(method)).toUpperCase() %>_ERFOLGREICH;
-    constructor(<%= responseparams %><% if (responseparams!='' && requestparams !='') {%>, <% } %><%= requestparams %><% if (responseparams !='') {%>, <% } %><%= optPayload %>) {}
+    constructor(<%= responseparams %><% if (responseparams!='' && requestparams !='') {%>, <% } %><%= requestparams %><% if (responseparams !='') {%>, <% } %><%= optPayload %>, public ngrxManager: NgrxManagerConfig = null) {}
     }
-export class <%= classify(method) %>FehlerAction implements Action {
+export class <%= classify(method) %>FehlerAction implements NgrxManagerAction {
     readonly type = <%= underscore(classify(method)).toUpperCase() %>_FEHLER;
-    constructor(public payload: any<% if (requestparams !='') {%>, <% } %><%= requestparams %>, <%= optPayload %>) {}
+    constructor(public payload: any<% if (requestparams !='') {%>, <% } %><%= requestparams %>, <%= optPayload %>, public ngrxManager: NgrxManagerConfig = null) {}
     }
 
 
