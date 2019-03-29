027d338e12d40a357233658a1c9bf252396d966a
Torsten Zwoch <tz@softwarepioniere.de>
2019-03-29 10:16:52 +0100

    clean up

--- src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
+++ src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
@@ -9,7 +9,7 @@ import {Effect, Actions, ofType} from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import {filter, switchMap} from 'rxjs/operators';
+import {filter, map, switchMap} from 'rxjs/operators';
 
 import * as ac from './actions.<%= actiontype %>.<%= dasherize(methodWithoutType) %>';
 import * as api from '<%= importpath %>clients/<%= clientname %>';
@@ -22,9 +22,10 @@ export class <%= classify(clientname) %><%= classify(methodWithoutType) %>Effect
     @Effect()
         <%= classify(methodWithoutType) %>$: Observable<Action> = this.actions$.pipe(
             ofType(ac.<%= underscore(classify(method)).toUpperCase() %>),
-            filter((x: ac.<%= classify(method) %>Action) => {
+            map((x: ac.<%= classify(method) %>Action) => {
                 return this.ngrxManagerService.callRequest(ac.<%= underscore(classify(method)).toUpperCase() %>, x, RequestMethod.<%= actiontype.toUpperCase() %>, RequestType.Anfrage);
             }),
+            filter(x => typeof x !== 'boolean'),
             switchMap((x: ac.<%= classify(method) %>Action) => {
                 return this._<%= camelize(classify(service)) %>Service.<%= method %>(<%= requestparamsVariableNames %>)
                     .map((result: any) => {
