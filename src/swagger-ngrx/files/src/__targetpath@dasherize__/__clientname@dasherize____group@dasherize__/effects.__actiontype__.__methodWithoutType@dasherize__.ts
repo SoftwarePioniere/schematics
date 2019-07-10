e688f3a19b874bc7c1133cb4db2b948ac7441903
Torsten Zwoch <tz@softwarepioniere.de>
2019-07-10 08:58:16 +0200

    change imports of effects

--- src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
+++ src/swagger-ngrx/files/src/__targetpath@dasherize__/__clientname@dasherize____group@dasherize__/effects.__actiontype__.__methodWithoutType@dasherize__.ts
@@ -1,9 +1,3 @@
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/skip';
-import 'rxjs/add/operator/takeUntil';
 import { Injectable } from '@angular/core';
 import {Effect, Actions, ofType} from '@ngrx/effects';
 import { Action } from '@ngrx/store';
